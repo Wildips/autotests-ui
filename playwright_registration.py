26a3c6446f6873058369a360b52d7cd0721d0867
Wildips <zak125@mail.ru>
2025-12-09 00:24:43 +0300

    Работа с авторизацией в Playwright. Шаг 5.

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,32 +1,38 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
+# Открываем браузер с использованием Playwright
 with sync_playwright() as playwright:
-    # Открываем браузер и создаем новую страницу
+    # Запускаем Chromium браузер в обычном режиме (не headless)
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
 
-    # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    # Заполняем поле email
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
-    # Заполняем поле пользователь
-    password_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    password_input.fill("password")
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    # Заполняем поле пароль
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill('password')
 
-    # Нажимаем на кнопку Registration
-    login_button = page.get_by_test_id('registration-page-registration-button')
-    login_button.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    # Проверяем, что появилось сообщение об ошибке
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_be_visible()
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
 
-    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
+# Остальной код регистрации нового пользователя без изменений
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
     page.wait_for_timeout(5000)
