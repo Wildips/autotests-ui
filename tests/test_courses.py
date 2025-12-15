39f6dcd19be39b58a47816333b9824b5cf7ff5f7
Wildips <zak125@mail.ru>
2025-12-15 22:28:47 +0300

    Знакомство с PageComponent. Шаг 14.

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,26 +1,18 @@
-from playwright.sync_api import expect
 import pytest
+
+from pages.courses_list_page import CoursesListPage
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):  # Создаем тестовую функцию
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):  # Создаем тестовую функцию
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    # Проверить наличие и текст заголовка "Courses"
-    courses_header = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_header).to_be_visible()
-    expect(courses_header).to_have_text('Courses')
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
 
-    # Проверить наличие и текст блока "There is no results"
-    no_result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(no_result_text).to_have_text('There is no results')
+    courses_list_page.check_visible_courses_title()
 
-    # Проверить наличие и видимость иконки пустого блока
-    empty_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(empty_icon).to_be_visible()
+    courses_list_page.check_visible_create_course_button()
 
-    # Проверить наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
-    empty_block_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(empty_block_description).to_be_visible()
-    expect(empty_block_description).to_have_text("Results from the load test pipeline will be displayed here")
+    courses_list_page.check_visible_empty_view()
