ffc3226613d82888a0835430f1c9852f1ccc7653
Wildips <zak125@mail.ru>
2025-12-16 00:20:12 +0300

    Практикуемся в реализации PageComponent. Шаг 11.

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,52 @@
 import pytest
 
 from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(create_course_page: CreateCoursePage,
+                       courses_list_page: CoursesListPage):  # Создаем тестовую функцию
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+
+    create_course_page.check_visible_create_course_form(
+        title="",
+        estimated_time="",
+        description="",
+        max_score="0",
+        min_score="0"
+    )
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+
+    create_course_page.fill_create_course_form(
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright",
+        max_score="100",
+        min_score="10"
+    )
+    create_course_page.click_create_course_button()
+
+    courses_list_page.toolbar_view.check_visible()
+
+    courses_list_page.course_view.check_visible(
+        index=0,
+        title="Playwright",
+        estimated_time="2 weeks",
+        max_score="100",
+        min_score="10"
+    )
 
 
 @pytest.mark.courses
@@ -11,8 +57,6 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):  # Создае
     courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
 
     courses_list_page.check_visible_empty_view()
