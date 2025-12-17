6e1aa2f68710e205ff2eb515a02fa4797687dc01
Wildips <zak125@mail.ru>
2025-12-17 15:41:55 +0300

    Практикуемся в реализации PageComponent. Шаг 12.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,7 +11,7 @@ def test_successful_registration(
         dashboard_page: DashboardPage
 ):  # Теперь используем фикстуру
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
 
-    dashboard_page.check_dashboard_title_visible()
+    dashboard_page.dashboard_toolbar_view.check_visible()
