490d0840e513b218f02993690af6638e3f72f4b1
Wildips <zak125@mail.ru>
2025-12-17 23:07:13 +0300

    Знакомство с PageFactory. Шаг 12.

--- components/authentication/login_form_component.py
+++ components/authentication/login_form_component.py
@@ -1,29 +1,31 @@
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
+from elements.input import Input
+from elements.text import Text
 
 
 class LoginFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.title = page.get_by_test_id('authentication-ui-course-title-text')
-        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.title = Text(page, 'authentication-ui-course-title-text', 'Title')
+        self.email_input = Input(page, 'login-form-email-input', 'Email')
+        self.password_input = Input(page, 'login-form-password-input', 'Password')
 
     def fill(self, email: str, password: str):
         self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
+        self.email_input.check_have_value(email)
 
         self.password_input.fill(password)
-        expect(self.password_input).to_have_value(password)
+        self.password_input.check_have_value(password)
 
     def check_visible(self, email: str, password: str):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text('UI Course')
+        self.title.check_visible()
+        self.title.check_have_text('UI Course')
 
-        expect(self.email_input).to_be_visible()
-        expect(self.email_input).to_have_text(email)
+        self.email_input.check_visible()
+        self.email_input.check_have_text(email)
 
-        expect(self.password_input).to_be_visible()
-        expect(self.password_input).to_have_text(password)
+        self.password_input.check_visible()
+        self.password_input.check_have_text(password)
